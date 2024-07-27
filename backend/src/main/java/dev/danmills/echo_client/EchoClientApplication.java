a6818b8b03167c0f9b951825ec53de9639491c38
twentynineteen <21161833+twentynineteen@users.noreply.github.com>
2024-07-27 11:48:32 +0100

    init spring boot backend

---
+++ backend/src/main/java/dev/danmills/echo_client/EchoClientApplication.java
@@ -0,0 +1,13 @@
+package dev.danmills.echo_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoClientApplication.class, args);
+	}
+
+}
