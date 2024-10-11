bb8e1336c916c7dca4adb4d6e8b8068de705fabe
twentynineteen <21161833+twentynineteen@users.noreply.github.com>
2024-10-11 11:49:13 +0100

    departments service added

---
+++ backend/src/main/java/dev/danmills/echo_client/api/controller/DepartmentController.java
@@ -0,0 +1,62 @@
+package dev.danmills.echo_client.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.echo360.sdk.model.objects.Department;
+import com.echo360.sdk.model.requests.ListRequest;
+import com.echo360.sdk.util.Echo360Exception;
+
+import dev.danmills.echo_client.persistence.entity.EchoLogger;
+import dev.danmills.echo_client.service.RESTDepartmentService;
+
+@RestController
+public class DepartmentController {
+   private final RESTDepartmentService restDepartmentService;
+   private static final EchoLogger log = new EchoLogger();
+
+   public DepartmentController(RESTDepartmentService restDepartmentService) {
+      this.restDepartmentService = restDepartmentService;
+   }
+
+   /**
+   * Get all of the departments.
+   *
+   * @return the list of entities
+   */
+  @GetMapping("/departments")
+  @ResponseBody
+  public ListRequest<Department> getDepartments() throws Echo360Exception {
+   log.logString("Attempting to get departments");
+   return restDepartmentService.getDepartments();
+  }
+
+   /**
+   * Get one department by ID.
+   *
+   * @param id is the id of the entity
+   * @return the course entity
+   */
+   @GetMapping("/departments/{id}")
+   @ResponseBody
+   public Department getDepartmentById(@PathVariable String id) throws Echo360Exception {
+      return restDepartmentService.getDepartmentById(id);
+
+   }
+
+   /**
+   * Get a list of departments by the organization ID.
+   *
+   * @param id is the id of the entity
+   * @return the course entity
+   */
+  @GetMapping("/departments/organizations/{id}")
+  @ResponseBody
+  public ListRequest<Department> getDepartmentsByOrg(@PathVariable String id) throws Echo360Exception {
+     return restDepartmentService.getDepartmentsByOrg(id);
+
+  }
+
+}
---
+++ backend/src/main/java/dev/danmills/echo_client/service/RESTDepartmentService.java
@@ -0,0 +1,68 @@
+package dev.danmills.echo_client.service;
+
+import org.springframework.stereotype.Service;
+
+import com.echo360.sdk.model.objects.Department;
+import com.echo360.sdk.model.requests.ListRequest;
+import com.echo360.sdk.services.DepartmentService;
+import com.echo360.sdk.util.Echo360Exception;
+
+import dev.danmills.echo_client.persistence.entity.EchoLogger;
+
+@Service
+public class RESTDepartmentService {
+   private final Echo360ApiService echo360ApiService;
+   private static final EchoLogger log = new EchoLogger();
+  
+   // Constructor 
+   public RESTDepartmentService(Echo360ApiService echo360ApiService) {
+         this.echo360ApiService = echo360ApiService;
+     
+      }
+
+      /**
+       * This Java function retrieves a list of departments with a specified limit and offset.
+      * 
+      * @return The `getDepartments` method returns a `ListRequest` object containing a list of `Department`
+      * objects.
+      */
+      public ListRequest<Department> getDepartments() throws Echo360Exception {
+         log.logString("GetDepartments called...");
+   
+         DepartmentService departmentService = new DepartmentService(echo360ApiService.echo360Api());
+         int limit = 500;
+         String offset = "";
+         ListRequest<Department> departmentList = departmentService.list(limit, offset);
+         return departmentList;
+      }   
+
+      /**
+      * This Java function retrieves a list of departments with a specified limit and offset, filtered by the organizationId parameter.
+      * 
+      * @return The `getDepartmentsByOrg` method returns a `ListRequest` object containing a list of `Department`
+      * objects.
+      */
+      public ListRequest<Department> getDepartmentsByOrg(String organizationId) throws Echo360Exception {
+         log.logString("GetDepartmentsByOrg called...");
+   
+         DepartmentService departmentService = new DepartmentService(echo360ApiService.echo360Api());
+         int limit = 500;
+         String offset = "";
+         ListRequest<Department> departmentList = departmentService.list(organizationId, limit, offset);
+         return departmentList;
+      }   
+
+      /**
+      * This Java function retrieves a department specified by it's ID.
+      * 
+      * @return The `getDepartmentById` method returns a `Department` object
+      */
+      public Department getDepartmentById(String departmentId) throws Echo360Exception {
+         log.logString("GetDepartmentById called...");
+   
+         DepartmentService departmentService = new DepartmentService(echo360ApiService.echo360Api());
+         Department departmentList = departmentService.get(departmentId);
+
+         return departmentList;
+      }   
+   }
