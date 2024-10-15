331ce7a9a4bb0841663ffb662c17767605df8cdf
twentynineteen <21161833+twentynineteen@users.noreply.github.com>
2024-10-15 10:27:41 +0100

    Added react - router - dom and routes for home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,60 @@
-import { ThemeProvider } from "@/components/theme-provider"
-import './App.css'
+import { ThemeProvider } from "@/components/theme-provider";
+import {
+  Link,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
+import './App.css';
+import Navbar from "./components/Navbar";
 
-import Navbar from "./components/Navbar"
-
-function App() {
-
-
+function Home() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Navbar/>
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 relative flex flex-1 items-center justify-left">
         <p className="text-3xl">Hello!</p>
       </div>
-    </ThemeProvider>
+  );
+}
+
+
+function App() {
+
+  return     (
+    <div>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      {/* Routes nest inside one another. Nested route paths build upon
+            parent route paths, and nested route elements render inside
+            parent route elements. See the note about <Outlet> below. */}
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes> 
+      </ThemeProvider>
+    </div>
   )
 }
 
 export default App
+
+
+function Layout() {
+  return (
+    <div>
+      <Navbar/>
+      <Outlet />
+    </div>
+  )
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
